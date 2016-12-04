a2b799f459e722a98c4bc0b668d5e47a89501937
Crystiam <crystiamkelle@gmail.com>
2016-12-04 18:40:49 -0200

    graf_4_9

--- GRAF_4_09_MQ.r
+++ GRAF_4_09_MQ.r
@@ -1,5 +1,5 @@
 ils_lns_data <- "/Users/CrystiamKelle/Documents/GitHub/met2_lns/data/FINAL_ILS_x_LNS/ils_lns_data.dat";
-tableILS_LNS <- read.table(ils_lns_data, header = TRUE, sep = ";");
+
 
 instanciasList <- list( "lslayout", "seemp", "imapd???1", "elm???1", "exim", "lucent", "dom4j", "pfcda_swing",
                  "jtreeview", "lwjgl???2.8.4", "krb5", "eclipse_jgit");
